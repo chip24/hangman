ab3041dc7a97d1f2cba0fe8a7b68528ac6721196
Chip Morgan <python.chipmorgan@gmail.com>
2023-04-06 08:44:51 -0500

    Create match method

--- hangman-testing.rb
+++ hangman-testing.rb
@@ -1,16 +1,58 @@
 f = File.open("google-10000-english-no-swears.txt")
 pick = f.select {|w| w.size > 4 && w.size <13}.sample
 puts pick
-pick_length = pick.length 
-puts pick_length
+p pick
+puts pick.length
+pick_minus_newline = pick.strip()
+p pick_minus_newline
+@pick_array = pick_minus_newline.split(//)
+p @pick_array
+@pick_length = pick_minus_newline.length
+puts @pick_length
 f.close
 
-puts "Your word has #{pick_length} letters."
-puts "_ " * pick_length
+puts "Your word has #{@pick_length} letters."
 
-puts "Choose a letter from A to Z."
-input = gets.chomp 
-    until input.count("^a-zA-Z") == 0 && input.count == 1
-    puts "Make sure you have only entered a single  letter between A and Z."
-    input = gets.chomp
+@board = "_ " * @pick_length
+puts @board
+
+@board_minus_newline = @board.strip()
+
+@board_array = @board_minus_newline.split(" ")
+p @board_array
+
+
+def player_guess()
+    puts "Choose a letter from A to Z."
+    @input = gets.chomp 
+        until @input.match("[a-zA-Z]") && @input.length == 1 
+        puts "Make sure you have only entered a single  letter between A and Z."
+        @input = gets.chomp
+        end
+    #puts @input
+end
+
+def match()
+    @pick_array.each_with_index do |element,idx|
+        #p element, idx
+        if element == @input
+            puts element
+            @board_array[idx] = element
+        end
+
+            #p @board_array
+
+
     end
+    p @board_array
+    puts @board_array.join(' ')
+end
+
+#def print_board()
+    
+
+player_guess()
+match()
+
+
+
