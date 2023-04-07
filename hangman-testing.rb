663e13a2c75ef657577f2a6c20133c71ee410289
Chip Morgan <python.chipmorgan@gmail.com>
2023-04-07 08:46:44 -0500

    Add a play_game method and @turn_count variable.

--- hangman-testing.rb
+++ hangman-testing.rb
@@ -11,6 +11,10 @@ p @pick_array
 puts @pick_length
 f.close
 
+@guess_array = []
+@wrong_guess_array = []
+@turn_count = 0
+
 puts "Your word has #{@pick_length} letters."
 
 @board = "_ " * @pick_length
@@ -29,7 +33,8 @@ def player_guess()
         puts "Make sure you have only entered a single  letter between A and Z."
         @input = gets.chomp
         end
-    #puts @input
+    @guess_array << @input
+    @turn_count += 1
 end
 
 def match()
@@ -38,21 +43,37 @@ def match()
         if element == @input
             puts element
             @board_array[idx] = element
+        
+=begin    
+        elsif element != @input
+            #puts "#{@input} is not in the puzzle."
+            @wrong_guess_array << @input unless @wrong_guess_array.include?(@input)
+=end   
+
+            
         end
 
+    
             #p @board_array
 
 
     end
     p @board_array
     puts @board_array.join(' ')
+    puts "This is the wrong guess array #{@wrong_guess_array}." 
+    puts "This is the right guess array #{@guess_array}."
 end
 
 #def print_board()
     
+def play_game()
 
-player_guess()
-match()
+    until @turn_count >= 26 do
+        player_guess()
+        match()
+    end
+end
 
+play_game()
 
 
